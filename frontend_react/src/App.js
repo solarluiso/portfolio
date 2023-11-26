5ed83bfccd42842d796b5dd6294791835883440a
Luis <solarluiso@gmail.com>
2023-11-25 20:27:36 -0800

    fetching data, bugs fixed

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -8,9 +8,9 @@ const App = () => (
   <div className="app">
     <Navbar />
     <Header />
-    {/* <About /> */}
-    {/* <Work /> */}
-    {/* <Skills /> */}
+    <About />
+    <Work />
+    <Skills />
     <Testimonial />
     <Footer />
   </div>
