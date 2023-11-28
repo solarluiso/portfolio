3cf5c7fd69efa976e488950154ebb72cddea4b77
Luis <solarluiso@gmail.com>
2023-11-27 20:22:51 -0800

    implemented testimonial style

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
 import { motion } from "framer-motion";
-import { HiChevronLeft, HiChecronRight } from "react-icons/hi";
 
 import { AppWrap, MotionWrap } from "../../wrapper";
 import { urlFor, client } from "../../client";
 import "./Testimonial.scss";
 
 const Testimonial = () => {
-  const [brands, setBrands] = useState([]);
-  const [testimonials, setTestimonials] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [testimonials, setTestimonials] = useState([]);
+  const [brands, setBrands] = useState([]);
 
   const handleClick = (index) => {
     setCurrentIndex(index);
@@ -25,7 +25,7 @@ const Testimonial = () => {
         setTestimonials(data);
       })
       .catch((error) => {
-        console.error("Error fetching data:", error);
+        console.error("Error fetching testimonials:", error);
       });
 
     client
@@ -34,23 +34,28 @@ const Testimonial = () => {
         setBrands(data);
       })
       .catch((error) => {
-        console.error("Error fetching data:", error);
+        console.error("Error fetching brands:", error);
       });
   }, []);
+
   return (
     <>
-      {testimonials.length && (
+      {testimonials.length > 0 && (
         <>
           <div className="app__testimonial-item app__flex">
             <img
-              src={urlFor(testimonials[currentIndex].imgurl)}
-              alt={testimonials[currentIndex].name}
+              src={urlFor(testimonials[currentIndex]?.imgurl).toString()}
+              alt={testimonials[currentIndex]?.name}
             />
             <div className="app__testimonial-content">
-              <p className="p-text">{testimonials[currentIndex].feedback}</p>
+              <p className="p-text">{testimonials[currentIndex]?.feedback}</p>
               <div>
-                <h4 className="bold-text">{testimonials[currentIndex].name}</h4>
-                <h5 className="p-text">{testimonials[currentIndex].company}</h5>
+                <h4 className="bold-text">
+                  {testimonials[currentIndex]?.name}
+                </h4>
+                <h5 className="p-text">
+                  {testimonials[currentIndex]?.company}
+                </h5>
               </div>
             </div>
           </div>
@@ -92,7 +97,10 @@ const Testimonial = () => {
             transition={{ duration: 0.5, type: "tween" }}
             key={brand._id}
           >
-            <img src={urlFor(brand.imgUrl)} alt={brand.name} />
+            {/* Add a check to ensure imgUrl is defined before using it */}
+            {brand.imgUrl && (
+              <img src={urlFor(brand.imgUrl).toString()} alt={brand.name} />
+            )}
           </motion.div>
         ))}
       </div>
